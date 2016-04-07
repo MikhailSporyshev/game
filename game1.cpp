c57a46a89e475ac020cd7f3652cffd943a60fc1b
MikhailSporyshev <mikhailsporyshev@gmail.com>
2016-04-07 11:02:00 +0300

    added matrix of chars

--- game1.cpp
+++ game1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
+const int X = 10, Y = 10;
+void getField(char** field);
+
+
 int main() {
+	char** field = new char*[Y];
+	for(int i = 0; i < Y; i++) {
+		field[i] = new char[X];
+	}
 	return 0;
+}
+
+
+void getField(char** field) {
+
 }
