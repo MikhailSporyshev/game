0b85f8c9b919d5d6d64308e352b4c5af8f6c3a5d
MikhailSporyshev <mikhailsporyshev@gmail.com>
2016-04-08 15:24:12 +0300

    monster class, monster walk

--- game1.cpp
+++ game1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,34 +11,32 @@ class Knight{
 	int x;
 	int y;
 public:
-	Knight(int inputX, int inputY):x(inputX), y(inputY) {
+	Knight(int inputY, int inputX):y(inputY), x(inputX) {
 		damage = 2;
 		health = 2;
 		speed = 2;
 	}
 
-	void walk(int step, char**& field, int xSize, int ySize) {
-		switch(step) {
-			case 1: if(field[x + 1][y] != '#' && xSize - 1 - x > 1) {
-				field[x][y] = '.';
-				x++;
-				field[x][y] = 'K';
-			}
-			case 2: if(field[x - 1][y] != '#' &&  x > 0) {
-				field[x][y] = '.';
-				x--;
-				field[x][y] = 'K';
-			}
-			case 3: if(field[x][y + 1] != '#' && ySize - 1 - y > 1) {
-				field[x][y] = '.';
-				y++;
-				field[x][y] = 'K';
-			}
-			case 4: if(field[x][y - 1] != '#' &&  y > 0) {
-				field[x][y] = '.';
-				y--;
-				field[x][y] = 'K';
-			}
+	void walk(int step, char** field, int xSize, int ySize) {
+		if(step == 1 && x < xSize - 1 && field[y][x + 1] != '#') {
+			field[y][x] = '.';
+			x++;
+			field[y][x] = 'K';
+		}
+		else if(step == 2 &&  x > 0 && field[y][x - 1] != '#') {
+			field[y][x] = '.';
+			x--;
+			field[y][x] = 'K';
+		}
+		else if(step == 3 && y < ySize - 1 && field[y + 1][x] != '#') {
+			field[y][x] = '.';
+			y++;
+			field[y][x] = 'K';
+		}
+		else if(step == 4 &&  y > 0 && field[y - 1][x] != '#') {
+			field[y][x] = '.';
+			y--;
+			field[y][x] = 'K';
 		}
 	}
 
@@ -49,29 +48,83 @@ public:
 	}
 };
 
-void getFieldAndKnight(char** field,int& x, int& y, int& xKnight, int& yKnight);
+
+class Monster{
+	int damage;
+	int health;
+	int speed;
+
+	int x;
+	int y;
+public:
+	Monster(int inputY, int inputX):y(inputY), x(inputX) {
+		damage = 2;
+		health = 2;
+		speed = 2;
+	}
+
+	void walk(char** field, int xSize, int ySize) {
+		int step = rand() % 4;
+		if(step == 0 && x < xSize - 1 && field[y][x + 1] != '#') {
+			field[y][x] = '.';
+			x++;
+			field[y][x] = 'M';
+		}
+		else if(step == 1 &&  x > 0 && field[y][x - 1] != '#') {
+			field[y][x] = '.';
+			x--;
+			field[y][x] = 'M';
+		}
+		else if(step == 2 && y < ySize - 1 && field[y + 1][x] != '#') {
+			field[y][x] = '.';
+			y++;
+			field[y][x] = 'M';
+		}
+		else if(step == 3 &&  y > 0 && field[y - 1][x] != '#') {
+			field[y][x] = '.';
+			y--;
+			field[y][x] = 'M';
+		}
+	}
+
+	int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
+};
+
+
+
+void getFieldAndKnightAndMonster(char** field,int& x, int& y, int& xKnight, int& yKnight, int& xMonster, int& yMonster);
 void displayField(char** field,int& x, int& y);
 
 int main() {
 	int x, y;
 	char** field = new char*[y];
 	int xKingt = 0, yKnight = 0;
+	int xMonster = 0, yMonster = 0;
 
 	int step = 0;
 
 	cout << "insert x y:" << endl;
 	cin >> x >> y;
 	
-	getFieldAndKnight(field, x, y, xKingt, yKnight);
+	getFieldAndKnightAndMonster(field, x, y, xKingt, yKnight, xMonster, yMonster);
 
 
-	Knight holyKnight(xKingt, yKnight);	
+	Knight holyKnight(yKnight, xKingt);	
+	Monster unholyMonster(yMonster, xMonster);
+
+	cout << holyKnight.getY() << " " << holyKnight.getX() << endl;
 
 
 	cout << "insert step: 1,2,3,4" << endl;
 	cin >> step;
 	while(step > 0) {
 		holyKnight.walk(step, field, x, y);
+		unholyMonster.walk(field, x, y);
 		displayField(field, x, y);
 		cin >> step;
 	}
@@ -79,15 +132,19 @@ int main() {
 	return 0;
 }
 
-void getFieldAndKnight(char** field, int& x, int& y, int& xKnight, int& yKnight) {
+void getFieldAndKnightAndMonster(char** field, int& x, int& y, int& xKnight, int& yKnight, int& xMonster, int& yMonster) {
 	cout << "insert field" << endl;
 	for(int i = 0; i < y; i++) {
 		field[i] = new char[x];
 		for(int j = 0; j < x; j++) {
 			cin >> field[i][j];
 			if(field[i][j] == 'K') {
-				xKnight = i;
-				yKnight = j;
+				yKnight = i;
+				xKnight = j;
+			}
+			else if(field[i][j] == 'M') {
+				yMonster = i;
+				xMonster = j;
 			}
 		}
 	}
