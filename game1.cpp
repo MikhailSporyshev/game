369398adef523c91279b9c31459e2967d6ba67e4
MikhailSporyshev <mikhailsporyshev@gmail.com>
2016-04-08 15:11:47 +0300

    walk improved

--- game1.cpp
+++ game1.cpp
@@ -16,29 +16,27 @@ public:
 		speed = 2;
 	}
 
-	void walk(int* steps, char**& field, int xSize, int ySize) {
-		for(int i = 0; i < 2; i++) {
-			switch(steps[i]) {
-				case 1: if(field[x + 1][y] != '#' && xSize - 1 - x > 1) {
-					field[x][y] = '.';
-					x++;
-					field[x][y] = 'K';
-				}
-				case 2: if(field[x - 1][y] != '#' &&  x > 0) {
-					field[x][y] = '.';
-					x--;
-					field[x][y] = 'K';
-				}
-				case 3: if(field[x][y + 1] != '#' && ySize - 1 - y > 1) {
-					field[x][y] = '.';
-					y++;
-					field[x][y] = 'K';
-				}
-				case 4: if(field[x][y - 1] != '#' &&  y > 0) {
-					field[x][y] = '.';
-					y--;
-					field[x][y] = 'K';
-				}
+	void walk(int step, char**& field, int xSize, int ySize) {
+		switch(step) {
+			case 1: if(field[x + 1][y] != '#' && xSize - 1 - x > 1) {
+				field[x][y] = '.';
+				x++;
+				field[x][y] = 'K';
+			}
+			case 2: if(field[x - 1][y] != '#' &&  x > 0) {
+				field[x][y] = '.';
+				x--;
+				field[x][y] = 'K';
+			}
+			case 3: if(field[x][y + 1] != '#' && ySize - 1 - y > 1) {
+				field[x][y] = '.';
+				y++;
+				field[x][y] = 'K';
+			}
+			case 4: if(field[x][y - 1] != '#' &&  y > 0) {
+				field[x][y] = '.';
+				y--;
+				field[x][y] = 'K';
 			}
 		}
 	}
@@ -51,37 +49,50 @@ public:
 	}
 };
 
+void getFieldAndKnight(char** field,int& x, int& y, int& xKnight, int& yKnight);
+void displayField(char** field,int& x, int& y);
+
 int main() {
 	int x, y;
 	char** field = new char*[y];
+	int xKingt = 0, yKnight = 0;
+
+	int step = 0;
 
 	cout << "insert x y:" << endl;
 	cin >> x >> y;
 	
-	bool flag = false;
+	getFieldAndKnight(field, x, y, xKingt, yKnight);
 
+
+	Knight holyKnight(xKingt, yKnight);	
+
+
+	cout << "insert step: 1,2,3,4" << endl;
+	cin >> step;
+	while(step > 0) {
+		holyKnight.walk(step, field, x, y);
+		displayField(field, x, y);
+		cin >> step;
+	}
+
+	return 0;
+}
+
+void getFieldAndKnight(char** field, int& x, int& y, int& xKnight, int& yKnight) {
 	cout << "insert field" << endl;
 	for(int i = 0; i < y; i++) {
 		field[i] = new char[x];
 		for(int j = 0; j < x; j++) {
 			cin >> field[i][j];
-			if(field[i][j] = 'K') {
-				Knight holyKnight(i, j);
-				flag = true;
+			if(field[i][j] == 'K') {
+				xKnight = i;
+				yKnight = j;
 			}
 		}
 	}
-
-	if(flag) {
-		cout << "insert 2 steps: 1 - right, 2- left, 3 - down, 4 - up" << endl;
-		int steps[2];
-		for(int i = 0; i < 2; i++) {
-			cin >> steps[i];
-		}
-
-	}
-
-
+}
+void displayField(char** field,int& x, int& y) {	
 	for(int i = 0; i < y; i++) {
 		for(int j = 0; j < x; j++) {
 			cout << field[i][j];
@@ -90,6 +101,4 @@ int main() {
 			}
 		}
 	}
-
-	return 0;
 }
